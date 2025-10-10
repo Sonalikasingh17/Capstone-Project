3ab96dc63c7ea967c50ffc4a338233fd238de634
sonalikasingh17 <singhsonalika5@gmail.com>
2025-10-10 23:06:16 +0530

    CICD run 12

--- flask_app/app.py
+++ flask_app/app.py
@@ -76,6 +76,7 @@ def normalize_text(text):
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
+
 dagshub_token = os.getenv("CAPSTONE_TEST")
 if not dagshub_token:
     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
@@ -115,6 +116,7 @@ PREDICTION_COUNT = Counter(
 model_name = "my_model"
 def get_latest_model_version(model_name):
     client = mlflow.MlflowClient()
+    # latest_version = client.get_latest_versions(model_name, stages=["Staging"])
     latest_version = client.get_latest_versions(model_name, stages=["Production"])
     if not latest_version:
         latest_version = client.get_latest_versions(model_name, stages=["None"])
